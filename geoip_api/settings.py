fcf421edb19e40ac3c85c09ccd1aa4775d76680b
Pawel Hamera <pjhamera@gmail.com>
2022-01-09 14:00:16 +0100

    hosts commit

--- geoip_api/settings.py
+++ geoip_api/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['geodatadrf.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['geoappapi.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
